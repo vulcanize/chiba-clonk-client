7fe5bcda83fea4fe4fe107f8456d9560a1d4ba61
nabarun <nabarun@deepstacksoft.com>
2022-04-04 12:35:16 +0530

    Add tests for querying bond

--- src/testing/helper.ts
+++ src/testing/helper.ts
@@ -1,5 +1,7 @@
-const DEFAULT_PRIVATE_KEY = '0451f0bd95c855d52e76cdc8dd06f29097b944bfef26d3455725157f9133f4e0';
-const DEFAULT_ADDRESS = 'ethm19n3je0lhuk0w9kmkftsuw4etn8lmpu3jjfayeh'
+const DEFAULT_PRIVATE_KEY = '794ce0bf3c75571416001c3415e69059aeba54038bcac8ce5b9792259e6d193b';
+const DEFAULT_ADDRESS = 'ethm10atmndy7sm46829rc3yr7cxqucgrz5e9jg58xp'
+
+export const wait = (time: number) => new Promise(resolve => setTimeout(resolve, time))
 
 export const getConfig = () => ({
   mockServer: process.env.MOCK_SERVER || false,
@@ -7,7 +9,8 @@ export const getConfig = () => ({
     chainId: process.env.CHIBA_CLONK_CHAIN_ID || 'ethermint_9000-1',
     privateKey: DEFAULT_PRIVATE_KEY,
     accountAddress: DEFAULT_ADDRESS,
-    endpoint: process.env.CHIBA_CLONK_ENDPOINT || 'http://localhost:1317',
+    restEndpoint: process.env.CHIBA_CLONK_REST_ENDPOINT || 'http://localhost:1317',
+    gqlEndpoint: process.env.CHIBA_CLONK_GQL_ENDPOINT || 'http://localhost:9473/api',
     fee: {
       amount: '20',
       denom: 'aphoton',
---
+++ src/util.ts
@@ -0,0 +1,79 @@
+/**
+ * Utils
+ */
+export class Util {
+  /**
+   * Sorts JSON object.
+   */
+  static sortJSON(object: any) {
+    if (object instanceof Array) {
+      for (let i = 0; i < object.length; i++) {
+        object[i] = Util.sortJSON(object[i]);
+      }
+      return object;
+    }
+    if (typeof object !== 'object' || object === null) return object;
+
+    let keys = Object.keys(object);
+    keys = keys.sort();
+    const newObject: {[key: string]: any} = {};
+
+    for (let i = 0; i < keys.length; i++) {
+      newObject[keys[i]] = Util.sortJSON(object[keys[i]]);
+    }
+    return newObject;
+  }
+
+  /**
+   * Marshal object into gql 'attributes' variable.
+   */
+  static toGQLAttributes(object: any) {
+    const vars: any[] = [];
+
+    Object.keys(object).forEach(key => {
+      let type: string = typeof object[key];
+      if (object[key] === null) {
+        vars.push({ key, value: { 'null': true } });
+      } else if (type === 'number') {
+        type = (object[key] % 1 === 0) ? 'int' : 'float';
+        vars.push({ key, value: { [type]: object[key] } });
+      } else if (type === 'string') {
+        vars.push({ key, value: { 'string': object[key] } });
+      } else if (type === 'boolean') {
+        vars.push({ key, value: { 'boolean': object[key] } });
+      } else if (type === 'object') {
+        const nestedObject = object[key];
+        if (nestedObject['/'] !== undefined) {
+          vars.push({ key, value: { 'reference': { id: nestedObject['/'] } } });
+        }
+      }
+    });
+
+    return vars;
+  }
+
+  /**
+   * Unmarshal attributes array to object.
+   */
+  static fromGQLAttributes(attributes: any[] = []) {
+    const res: {[key: string]: any} = {};
+
+    attributes.forEach(attr => {
+      if (attr.value.null) {
+        res[attr.key] = null;
+      } else if (attr.value.json) {
+        res[attr.key] = JSON.parse(attr.value.json);
+      } else if (attr.value.reference) {
+        // Convert GQL reference to IPLD style link.
+        const ref = attr.value.reference;
+        res[attr.key] = { '/': ref.id };
+      } else {
+        const { values, null: n, ...types } = attr.value;
+        const value = Object.values(types).find(v => v !== null);
+        res[attr.key] = value;
+      }
+    });
+
+    return res;
+  }
+}
